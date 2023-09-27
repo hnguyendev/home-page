427b65ace2f6977a86d63abf9adcbd67f828b230
hnguyendev <duchung682003@gmail.com>
2023-09-27 21:19:37 +0700

    first commit

---
+++ home-page/src/App.jsx
@@ -0,0 +1,13 @@
+import GlobalStyle from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <AppLayout />;
+    </>
+  );
+}
+
+export default App;
---
+++ home-page/src/ui/Header.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  background-color: var(--color-grey-0);
+  padding: 1.2rem 4.8rem;
+  border-bottom: 1px solid var(--color-grey-100);
+  grid-column: 1/-1;
+  height: 14rem;
+  background-color: pink;
+`;
+
+function Header() {
+  return <StyledHeader>HEADER</StyledHeader>;
+}
+
+export default Header;
